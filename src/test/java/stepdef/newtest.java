f233404cb0ef8d00942390dd34b94a1fc380d4d3
vamshi <vamshikrishnadeegoju@gmail.com>
2024-01-28 13:22:52 +0530

    2nd

---
+++ src/test/java/stepdef/newtest.java
@@ -0,0 +1,10 @@
+package stepdef;
+
+public class newtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/test/java/stepdef/steps.java
+++ src/test/java/stepdef/steps.java
@@ -1,5 +1,5 @@
 package stepdef;
 
 public class steps {
-
+//bvamm
 }
